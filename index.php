3fd9812316e5db74bb3b307a398233af1b0a11b7
Ray McMahan <ray.mcmahan1@gmail.com>
2015-05-10 00:27:52 -0500

    Finished Services mockup. Started on photo gallery and Contact.

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html class="skrollr skrollr-desktop">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Something Special Lawncare</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,400' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="hero" id="landing">
+    <div class="header">
+      <div class="branding">
+        <a href="#landing">
+          <span class="logo"><img src="/somethingspecial/assets/img/logo.png" alt="Something Special logo"></span>
+          
+          <h1>Something<br />Special</h1>
+        </a>
+      </div>
+      
+      <div>
+        <p class="call-us">Call us at (540) 986-6731</p>
+        
+        <!-- nav menu -->
+        <div class="navmenu">
+          <a href="#services">Services</a>
+          <a href="#photos">Photos</a>
+          <a href="contact">Contact</a>
+        </div>
+      </div>
+    </div>
+    
+    <div class="blurb">
+      <h2>We do <strong>something special</strong> to your lawn.</h2>
+      
+      <p>Blurb about who we are. Edit this. Something Special serves the landscaping needs of the Roanoke, VA area. We do mowing, pruning, snow removal, and more. See examples of our work in Photos or check out our Services and prices.</p>
+    </div>
+  </div>
+  
+  <section class="services" id="services">
+    <div class="servicesTextWrap">
+      <h2>Services</h2>
+      
+      <p><strong>Landscape design, maintenance, snow removal -we can do it all.</strong> More text here about what kinds of Services Something Special offers. Pricing info can go here.</p>
+      <p>A call to action. Call us to get a quote. Or a freebie for the potential customer.</p>
+      
+      <form>
+        <label>Name*</label>
+        <input type="text" placeholder="Name" />
+        
+        <label>Email*</label>
+        <input type="text" placeholder="Email" />
+        
+        <label>Phone</label>
+        <input type="text" placeholder="(123) 456-7890" />
+        
+        <textarea placeholder="Maybe we can add a contact form here"></textarea>
+      </form>
+    </div>
+  </section>
+  
+  <section class="photos" id="photos">
+    <h2>Photos</h2>
+    
+  </section>
+  
+  <section class="contact" id="contact">
+    <div>
+      <img src="/somethingspecial/assets/img/phone-icon.png" class="contact-icon" />
+      <p>(540)986-6731</p>
+    </div>
+  </section>
+  
+  <!--script type="text/javascript" src="/somethingspecial/assets/js/3rdparty/skrollr.min.js"></script>
+  <script type="text/javascript">
+    var s = skrollr.init();
+  </script-->
+</body>
+</html>
